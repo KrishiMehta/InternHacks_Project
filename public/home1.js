eb3e4609cd9a23a91e951e5013278a3d8dbb2346
chirag sharma <chiragpsharma27@gmail.com>
2024-07-22 00:37:01 +0530

    Initial commit

---
+++ public/home1.js
@@ -0,0 +1,18 @@
+
+   
+
+
+function openSignupPage() {
+    window.location.href = "http://127.0.0.1:5500/internhacks_signup/public/index.html"
+}
+
+function openLoginPage() {
+    window.location.href = "http://127.0.0.1:5500/internhacks_signup/public/login.html"
+}
+
+
+
+
+// function redirectprofile(){
+
+// }
